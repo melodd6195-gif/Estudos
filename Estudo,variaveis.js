b4003aa0eebac97ff77fd847c0176ad017cabc9b
Dennis <melodd6195@gmail.com>
2026-05-16 18:26:12 -0300

    .

---
+++ Estudo,variaveis.js
@@ -0,0 +1,18 @@
+const nome = "Dennis"
+const AnoDeNascimento = 2010
+let idade = 16
+let AnoAtual = 2026
+let salario = 1400
+let imposto = 400
+
+let Idade = AnoAtual - AnoDeNascimento
+let DinheiroQueSobrou = salario - imposto
+
+console.log(nome)
+console.log(AnoDeNascimento)
+console.log(idade)
+console.log(AnoAtual)
+console.log(salario)
+console.log(imposto)
+console.log(Idade)
+console.log(DinheiroQueSobrou)
