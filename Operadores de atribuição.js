5e6e96569b4d4f1a9f17f76f8c79293c35c041b7
Dennis <melodd6195@gmail.com>
2026-05-16 19:48:16 -0300

    .

--- Operadores de atribuição.js
+++ Operadores de atribuição.js
@@ -8,8 +8,33 @@
 
 //--------------exemplo-------------//
 
-let price = 350;
-console.log(price);
+let dinheiro = 200
 
-price += 50;
-console.log(price);
+dinheiro -= 50
+
+console.log(`o saldo é de: R$${dinheiro}`)
+
+let pontos = 10
+
+pontos += 5
+
+console.log(`a quantidade de pontos é: ${pontos}`)
+
+let nivel = 3
+
+nivel *= 2
+
+console.log(`o seu nivel é: ${nivel}`)
+
+let vida = 100
+
+vida /= 2
+
+console.log(`A sua vida é: ${vida}`)
+
+let pontos2 = 10
+
+pontos2 +=5 - 4
+pontos2 *= 2
+
+console.log(`o resultado do ponto é de: ${pontos2}`)
