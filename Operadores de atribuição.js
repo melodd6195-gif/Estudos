fc02689b50be1477a25e9dcaa744d56cdec40cce
Dennis <melodd6195@gmail.com>
2026-05-16 19:18:47 -0300

    .

---
+++ Operadores de atribuição.js
@@ -0,0 +1,15 @@
+//------atribuicao-------//
+
+// = atribuicao simples
+// += adicao combinada
+// -= subtracao combinada
+// *= multiplicacao combinada
+// /= divisao combinada
+
+//--------------exemplo-------------//
+
+let price = 350;
+console.log(price);
+
+price += 50;
+console.log(price);
