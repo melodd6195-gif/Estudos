422cf14ffbdb6cb4289a967be510a6677c4cf06f
Dennis <melodd6195@gmail.com>
2026-05-16 19:01:54 -0300

    .

---
+++ OperadoresAritmeticos.js
@@ -0,0 +1,48 @@
+// operadores aritméticos
+// + adicao
+// * multiplicacao
+// - subtracao
+// / divisao
+// % modulo
+
+//----------------------Exercio 1-------------------------//
+
+//let dinheiro = 200
+//let gasto = 75
+
+//let sobra = dinheiro - gasto
+
+//console.log(`o dinheiro que sobrou foi de: R$${sobra}`)
+
+//-----------------------exercicio 2-------------------------//
+
+//let pizza = 12
+//let pessoas = 5
+
+//let PizzaPorPessoas = pizza / pessoas
+
+//console.log(`Ficou ${PizzaPorPessoas} pizza por pessoa`)
+
+//------------------------exercicio 3-----------------------//
+
+//let pontos = 10
+//let Pontos = (pontos + 5 -4) * 2
+
+ //console.log(`a quantidade de pontos que sobrou foi de:${Pontos}`)
+
+ //-----------------------exercicio 4-----------------------//
+
+ //let a = 6
+ //let b = 3
+ //let c = 2
+
+ //let valorA = a + b * c
+ //let valorB = (a + b) * c
+ //let valorC = a * b + c
+
+
+ //console.log(`o Valor A deu: ${valorA}`)
+ //console.log(`o Valor B deu: ${valorB}`)
+ //console.log(`o valor C deu: ${valorC}`)
+
+ //----------------------finalizado--------------------------//
