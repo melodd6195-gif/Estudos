587aa2a623e56e544fa08e2a60216a2dea1fa08c
Dennis <melodd6195@gmail.com>
2026-05-17 18:45:39 -0300

    ..

---
+++ Operadores de comparação.js
@@ -0,0 +1,21 @@
+// comparação //
+
+// > Maior que
+// < menor que
+// >= maios ou igual a
+// <= menor ou igual 
+// == igual a
+// === estritamento igual a
+// != diferente de
+// !+ estritamente diferente de
+
+console.log(4 > 3)
+console.log(4 < 3)
+console.log(4 >= 4)
+console.log(4 >= 5)
+console.log(4 <= 5)
+console.log(4 <= 3)
+console.log(4 == "4")
+console.log(4 === "4")
+console.log(4 != "4")
+console.log(4 !== "4")
---
+++ Operadores lógicos.js
@@ -0,0 +1,99 @@
+// logicos //
+
+// && //
+
+//const pizza = true
+//const xsalada = true
+
+//console.log(pizza && xsalada)
+
+//--//--//
+
+//const xsalada = true
+//const sorvete = true
+
+//console.log(sorvete || xsalada)
+
+//------ ! -------/
+
+//const xsalada = false
+
+//
+
+//exemplos//
+
+//const EscovarOsDentes = true;
+//const TomarBanho = true;
+
+//const Pronto = EscovarOsDentes && TomarBanho;
+
+//console.log(Pronto)
+
+//exemplo2//
+
+//const LavarLouca = false;
+//const LimparCasa = true;
+
+//const pronto = LavarLouca || LimparCasa
+
+//console.log(pronto)
+
+//exemplo 3//
+
+//let numero = 10;
+//let numero2 = 11;
+//let numero3 = 9;
+
+//resultado = numero >= numero2
+//resultado1 = numero >= numero3
+//resultado2 = numero3 <= numero2
+
+//console.log(resultado)
+//console.log(resultado1)
+//onsole.log(resultado2)
+
+//exemplo 2//
+
+//let idade = 17
+//let idade2 = 18
+
+//let porta = idade >= idade2 ? "pode entrar" : "nao pode entrar";
+
+//console.log(porta)
+
+//exemplo3//
+
+//let saldo = 150
+//let saldo2 = 100
+
+//let resultado = saldo >= saldo2 ? "saldo suficiente" : "saldo baixo"
+
+//console.log(resultado)
+
+//exemplo4//
+
+//let numero = 3;
+
+//let resultado = numero % 2 == 0? "par" : "impar"
+
+//console.log(resultado)
+
+//let senha = "1234"
+
+//let resultado = senha == "1234" ? "liberado" : "acesso negado"
+
+//console.log(resultado)
+
+
+//let idade = 18
+
+//let idadepermitida = idade >= "18" ? "permitido dirigir" : "nao pode dirigir"
+
+//console.log(idadepermitida);
+
+//let moedas = 50;
+
+//let soma = moedas += 25
+//let resultado = moedas >= 70 ? "pode comprar" : "nnao pode comprar"
+
+//console.log(resultado)
