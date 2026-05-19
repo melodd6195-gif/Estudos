95e2b28879d5c08cc84ff17101349b6b02d50ad6
Dennis <melodd6195@gmail.com>
2026-05-18 21:42:18 -0300

    .

---
+++ exercicio/ex1.js/ex4.js
@@ -0,0 +1,7 @@
+let moedas = 40
+
+let resultado = moedas += 35;
+resultado = moedas >= 100 ? "rico" :
+resultado = moedas >= 70 ? "pode comprar item raro" : "poucas moedas"
+
+console.log(resultado)
---
+++ exercicio/ex1.js/ex5.js
@@ -0,0 +1,9 @@
+let vida = 0;
+
+vida = vida - 2;
+vida = vida - 1;
+
+let resultado = vida = vida == 1 ? "perigo" : 
+vida > 1 ? "vivo" : "GameOver"
+
+console.log(resultado)
