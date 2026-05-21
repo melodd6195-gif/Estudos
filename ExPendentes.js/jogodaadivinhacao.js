80a2c82d8823ce5f5e1d5d421b0f4a26d8cc9ad6
Dennis <melodd6195@gmail.com>
2026-05-20 22:11:42 -0300

    .

---
+++ ExPendentes.js/jogodaadivinhacao.js
@@ -0,0 +1,13 @@
+// Projeto: Jogo de Adivinhação
+//
+// Crie uma variável chamada numeroSecreto com o valor que quiser
+// Crie uma variável chamada chute com o valor que quiser
+//
+// Se o chute for igual ao numeroSecreto exiba "acertou!"
+//
+// Se o chute for maior que o numeroSecreto exiba "muito alto!"
+//
+// Se o chute for menor que o numeroSecreto exiba "muito baixo!"
+//
+// Testa com valores diferentes para ver os 3 resultados acontecendo!
+// Por exemplo: tenta acertar, errar pra cima e errar pra baixo
