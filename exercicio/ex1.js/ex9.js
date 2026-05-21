ee5044035641c83fa6d38653fdf8083f8b4fa716
Dennis <melodd6195@gmail.com>
2026-05-20 22:08:28 -0300

    .

---
+++ exercicio/ex1.js/ex9.js
@@ -0,0 +1,13 @@
+// Exercício 1
+// Crie uma variável chamada temperatura com o valor 38
+// Se a temperatura for maior que 37, exiba "febre", senão exiba "normal"
+
+let temperatura = 37
+
+if(temperatura >= 37){
+  console.log("febre")
+}else if( temperatura <= 35) {
+   console.log("hipotermia")}
+else{
+  console.log("normal")
+}
